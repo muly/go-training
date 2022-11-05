b8f63fccb2c561cfc0175ac84cf88aebc6eb8197
muly <msrinivasareddy@gmail.com>
2022-11-05 03:27:51 -0400

    cleanup

--- 2022/2/1array/array1.go
+++ 2022/2/1array/array1.go
@@ -1,10 +1,9 @@
 //
 package main
 
-import "fmt"
-
 func main() {
 
+	// //
 	// { // initializing: declare and initialize
 	// 	var a [4]int
 	// 	a = [4]int{1,2,3,4}
@@ -15,24 +14,25 @@ func main() {
 	// 	// fmt.Println(a[4])
 	// }
 
+	// //
 	// { // initializing: declare and initialize in same line
 	// 	var a [4]int = [4]int{1, 2, 3, 4}
 	// 	fmt.Println(a)
 	// }
 
+	// //
 	// { // initializing: declare and initialize in same line, inferring the datatype
 	// 	var a = [4]int{1, 2, 3, 4}
 	// 	fmt.Println(a)
 	// }
 
-
-
-
+	// //
 	// { // initializing: short hand form
 	// 	a := [4]int{1, 2, 3, 4}
 	// 	fmt.Println(a)
 	// }
 
+	// //
 	// { // assigning one array to another array
 	// 	a := [4]int{1, 2, 3, 4}
 	// 	var b [4]int
@@ -40,6 +40,7 @@ func main() {
 	// 	fmt.Println(a, b)
 	// }
 
+	// //
 	// { // assigning one array to another array of different length. array has the length as part of the data type
 	// 	a := [4]int{1, 2, 3, 4}
 	// 	var b [5]int
@@ -47,37 +48,43 @@ func main() {
 	// 	fmt.Println(a, b)
 	// }
 
-	// { // reinitializing the array 
+	// //
+	// { // reinitializing the array
 	// 	a := [4]int{1, 2, 3, 4}
 
-	// 	a = [4]int{10,30,50,60}
+	// 	a = [4]int{10, 30, 50, 60}
 	// 	fmt.Println(a)
 	// }
 
+	// //
 	// { // reinitializing the array: with different length
 	// 	a := [4]int{1, 2, 3, 4}
 
-	// 	a = [4]int{10,30,50,60}
+	// 	a = [4]int{10, 30, 50, 60}
 	// 	fmt.Println(a)
 
-	// 	a = [5]int{10,30,50,60, 70} // ERROR: 
+	// 	a = [5]int{10, 30, 50, 60, 70} // ERROR:
 	// 	fmt.Println(a)
 	// }
 
+	// //
 	// // un initialized variable
 	// var i int
 	// fmt.Println(i)
 
+	// //
 	// { // un initialized array: initialized all elements with zero value
 	// 	var a [4]int
 	// 	fmt.Println(a) // [0 0 0 0]
 	// }
 
+	// //
 	// { // len and cap system functions
 	// 	a := [4]int{1, 2, 3, 4}
 	// 	fmt.Println(len(a), cap(a))
 	// }
 
+	// //
 	// { // accessing individual element
 	// 	a := [4]int{1, 2, 3, 4}
 	// 	fmt.Println(a[2])
@@ -85,27 +92,29 @@ func main() {
 	// 	fmt.Println(a[3])
 	// }
 
+	// //
 	// { // accessing individual element: outside the boundary
 	// 	a := [4]int{1, 2, 3, 4}
-	// 	// fmt.Println(a[4]) //ERROR: invalid argument: array index 4 out of bounds [0:4]
+	// 	fmt.Println(a[4]) //ERROR: invalid argument: array index 4 out of bounds [0:4]
 	// }
 
+	// //
 	// { // changing  individual element:
 	// 	a := [4]int{1, 2, 3, 4}
-	// 	a[1]=100
+	// 	a[1] = 100
 	// 	fmt.Println(a)
 	// }
 
 	// { // for loop: length is hard coded
 	// 	a := [4]int{1, 2, 3, 4}
-	// 	for i:=0; i<4; i++ {
+	// 	for i := 0; i < 4; i++ {
 	// 		fmt.Println(a[i])
 	// 	}
 	// }
 
 	// { // for loop: using len()
 	// 	a := [4]int{1, 2, 3, 4}
-	// 	for i:=0; i<len(a); i++ {
+	// 	for i := 0; i < len(a); i++ {
 	// 		fmt.Println(a[i])
 	// 	}
 	// }
