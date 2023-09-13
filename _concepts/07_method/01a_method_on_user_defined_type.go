f0524b62173a579e4c3de554a19f5e5e309e9cad
muly <msrinivasareddy@gmail.com>
2023-09-13 17:56:47 -0400

    more interface changes

--- _concepts/07_method/01a_method_on_user_defined_type.go
+++ _concepts/07_method/01a_method_on_user_defined_type.go
@@ -5,12 +5,12 @@ import "fmt"
 type myString string // user defined type
 
 func (s myString) prefix(p string) string {
-	return fmt.Sprintf("%s%s",p , s)
+	return fmt.Sprintf("%s%s", p, s)
 }
 
 // method on golang system type is not allowed
 func (s string) prefix(p string) string { // ERROR: cannot define new methods on non-local type string
-	return fmt.Sprintf("%s%s",p , s)
+	return fmt.Sprintf("%s%s", p, s)
 }
 
 func main() {
